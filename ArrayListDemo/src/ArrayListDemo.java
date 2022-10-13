116323efd7b07815dc422b63a11a06225da0fbdc
priyanka <priyankapole25@gmail.com>
2022-10-13 13:59:02 +0530

    first commit

---
+++ ArrayListDemo/src/ArrayListDemo.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+	
+	public static void main(String[] args) {
+		
+		ArrayList<String>list=new ArrayList<String>();
+		
+		list.add("priyanka");
+		list.add("sonali");
+		list.add("swapnil");
+		System.out.println(list);
+	}
+
+}
+
